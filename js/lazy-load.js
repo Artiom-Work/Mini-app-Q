5ea2a3d6510ece08cad4d53c9912c17e30953142
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-05-05 18:37:34 +0300

    performance test. hidden sections

--- js/lazy-load.js
+++ js/lazy-load.js
@@ -6,23 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
 	const lazyLoadElements = document.querySelectorAll('.lazy-load');
 	console.log(lazyLoadElements);
 
-	const observer = new IntersectionObserver((entries) => {
-		// entries - массив элементов, которые пересекли viewport
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-				console.log(entry.target);
-				// Если элемент пересек viewport
-				// Удаляем класс lazy-load
-				entry.target.style.display = "block";
-				// Отписываемся от наблюдения
-				observer.unobserve(entry.target);
-			}
-		});
+	lazyLoadElements.forEach(firstLoadElement => {
+		firstLoadElement.style.display = "none";
 	});
 
-	lazyLoadElements.forEach(element => {
-		observer.observe(element);
-	});
 });
 
 
