ac3d142dce72f598b60f149577ad993990a76f32
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-05-05 18:40:43 +0300

    performance test. hidden sections - 2

--- js/lazy-load.js
+++ js/lazy-load.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	const lazyLoadElements = document.querySelectorAll('.lazy-load');
 	console.log(lazyLoadElements);
 
-	lazyLoadElements.forEach(firstLoadElement => {
-		firstLoadElement.style.display = "none";
-	});
+	// lazyLoadElements.forEach(firstLoadElement => {
+	// 	firstLoadElement.style.display = "none";
+	// });
 
 });
 
